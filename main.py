33b10551de28fd1ff4c00cec7daaddac2f953e43
Charles-Hong520 <56359884+Charles-Hong520@users.noreply.github.com>
2024-03-06 19:48:33 -0800

    commented out stuff

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import requests,datetime,calendar
-
-season = "Winter"
-year = 2024
-
 CLIENT_ID = "e35f268cb013b3bafac987ca19a81b58"
-username = "basketbals"
+
+# season = "Winter"
+# year = 2024 
 # seasonal_url = f"https://api.myanimelist.net/v2/anime/season/{year}/{season.lower()}?limit=2"
 # top_airing_url = "https://api.myanimelist.net/v2/anime/ranking?ranking_type=airing&limit=13"
-user_watching_url = f'https://api.myanimelist.net/v2/users/{username}/animelist?status=watching&limit=15&nsfw=true'
-
 def getAnimeDetailsURL(id):
 	return f'https://api.myanimelist.net/v1/anime/{id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics'
+
+
+username = "basketbals" # ENTER USERNAME HERE
+user_watching_url = f'https://api.myanimelist.net/v2/users/{username}/animelist?status=watching&limit=15&nsfw=true'
 response = requests.get(user_watching_url, headers = {'X-MAL-CLIENT-ID': CLIENT_ID})
 jsonobject = response.json()
-print(len(jsonobject["data"]))
+print(len(jsonobject["data"]), "total animes")
 l=[]
 for anime in jsonobject["data"]:
 	anime_url = getAnimeDetailsURL(anime["node"]["id"])
