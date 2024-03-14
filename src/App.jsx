ea9af76a4f180cf9fdb792fdef19f571f0db1c4f
Charles-Hong520 <56359884+Charles-Hong520@users.noreply.github.com>
2024-03-14 14:53:35 -0700

    removed frontend folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ReactDOM from 'react-dom/client';
 import Week from './Week.jsx'
 import Form from './Form.jsx'
 function App() {
