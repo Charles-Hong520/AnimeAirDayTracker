5ade85320e6b3d6acdf21cea9b24a3dd6305778b
KunalMittal-Empire <kunal.mittal@empireio.com>
2024-03-14 10:54:12 -0700

    Finished api

--- backend/anime_api.py
+++ backend/anime_api.py
@@ -1,11 +1,14 @@
 from typing import Union
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os
 import requests
 import datetime
 import calendar
 from anime_api_calls import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 
@@ -19,6 +22,8 @@ def read_root():
 def get_airing(username: str) -> dict:
     user_watching_url = getAnimeList(username)
     anime_dict = requests.get(user_watching_url, headers = {'X-MAL-CLIENT-ID': CLIENT_ID}).json()
+    if "error" in anime_dict:
+        raise HTTPException(status_code=404, detail=anime_dict["error"])
     l = []
     for anime in anime_dict["data"]:
         anime_url = getAnimeDetailsURL(anime["node"]["id"])
